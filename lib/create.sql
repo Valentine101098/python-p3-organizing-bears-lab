73f2b9ea86a8285c89041fb80264522d98a30bc3
Valentine Wanjiru <kt116tabitha@gmail.com>
2025-07-01 13:04:43 +0300

    performed CRUD operations using sql

---
+++ lib/create.sql
@@ -0,0 +1,9 @@
+create table bears (
+    id integer primary key,
+    name text,
+    age integer,
+    sex text,
+    color text,
+    temperament text,
+    alive boolean
+)
---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+insert into bears(name, age, sex, color,temperament,alive) values
+("Mr. Chocolate", 12, "M", "Brown", "Choleric", true),
+("Rowdy", 6, "F", "White", "Sanguine", true),
+("Tabitha", 13, "F", "White", "Melancholic", true),
+("Sergent Brown", 24, "M", "Black", "Phlegmatic",false),
+("Melissa", 10, "F", "Brown", "Sanguine", true),
+("Grinch", 31, "M", "White", "Melancholic", false),
+("Wendy",21, "F", "Black", "Choleric", true),
+(null , 15, "M", "Brown", "Choleric", true);
